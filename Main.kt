b0eadb58cef6fbe8579dd06b59ecd0a67dbc0476
buny10 <shahhulash@gmal.com>
2024-01-15 12:34:33 +0545

    first commit

---
+++ Main.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    val age = 22
+
+    val maxintegervalue = Int.MAX_VALUE
+    val minintvalue = Int.MIN_VALUE
+
+    println("max value $maxintegervalue ")
+    println(" min value $minintvalue")
+
+    val maxbyte: Byte = Byte.MAX_VALUE
+    val minbyte: Byte = Byte.MIN_VALUE
+
+    println("max is $maxbyte min is $minbyte")
+     val short: Short = Short.MAX_VALUE
+    val mshort: Short = Short.MIN_VALUE
+    println("maxshort is $short minshort is $mshort")
+
+    val maxlong: Long = Long.MAX_VALUE
+    val minlong: Long = Long.MIN_VALUE
+     println(" maxlong is $maxlong minlong is $minlong")
+
+     val mynum = 299884844848484
+
+    val num = 2.6
+    val mnum = 2.5f
+     println("$num is also to $mnum")
+
+    val myCharValue: Char = 'a'
+     println("$myCharValue")
+    val boolean: Boolean =true
+    println("$boolean")
+
+
+}
